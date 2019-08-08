d61b097dd2672b3dc47a19ab9d1e480c8f6f9d00
briceyokoyama <briceyokoyama@gmail.com>
2019-08-07 19:23:20 -0700

    add simple template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request) :
-  return HttpResponse('<h1>Blog Home</h1>')
+posts = [
+  {
+    'author': 'Brice',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2018'
+  },
+  {
+    'author': 'Someone',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2018'
+  }
+]
 
-def about(request) :
-  return HttpResponse('<h1>Blog About</h1>')
+def home(request):
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
+
+def about(request):
+  return render(request, 'blog/about.html')
